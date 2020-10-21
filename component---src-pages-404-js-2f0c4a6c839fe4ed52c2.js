5109c523f14b39cf77cf46ce51e5ec25cce486e7
Dominique Ng <dominique@u.nus.edu>
2020-10-21 14:26:38 +0800

    Updates

---
+++ component---src-pages-404-js-2f0c4a6c839fe4ed52c2.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("tioU"),o=n("DDoQ");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2f0c4a6c839fe4ed52c2.js.map
---
+++ component---src-pages-page-2-js-7a6815187f14db8be4ba.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{p5nM:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),o=n.n(a),l=n("Wbzz"),c=n("tioU"),r=n("DDoQ");t.default=function(){return o.a.createElement(c.a,null,o.a.createElement(r.a,null,o.a.createElement("h1",null,"Hi from the second page"),o.a.createElement("p",null,"Welcome to page 2"),o.a.createElement(l.Link,{to:"/"},"Go back to the homepage")))}}}]);
+//# sourceMappingURL=component---src-pages-page-2-js-7a6815187f14db8be4ba.js.map
