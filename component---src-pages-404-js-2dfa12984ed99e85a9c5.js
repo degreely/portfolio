406a7ad487372481b09b61429263c7778a9a4804
Dominique Ng <dominique@u.nus.edu>
2020-11-09 18:53:24 +0800

    Updates

---
+++ component---src-pages-404-js-2dfa12984ed99e85a9c5.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[5],{w2l6:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),u=n("tioU"),o=n("DDoQ");t.default=function(){return l.a.createElement(u.a,null,l.a.createElement(o.a,null,l.a.createElement("h1",null,"NOT FOUND"),l.a.createElement("p",null,"You just hit a route that doesn't exist... the sadness.")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-2dfa12984ed99e85a9c5.js.map
