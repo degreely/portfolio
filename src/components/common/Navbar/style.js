b49ef193fc8a067a1eb3c4dc097d24f195bbb376
Dominique Ng <dominique@u.nus.edu>
2020-11-10 21:40:27 +0800

    Bold main navbar

--- src/components/common/Navbar/style.js
+++ src/components/common/Navbar/style.js
@@ -42,7 +42,7 @@ export const NavListWrapper = styled.div`
 export const NavItem = styled.li`
   margin: 0 0.75em;
   font-family: ${props => props.theme.font.primary};
-  font-weight: 500;
+  font-weight: 600;
   ${props => props.theme.font_size.small};
 
   a {
